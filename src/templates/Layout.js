4eb2167e2eb867d0d117cbca35252b0d9a85a78b
Cookieman2002 <mcl2002@outlook.dk>
2023-07-10 21:44:33 +0200

    opstart med navigation

--- src/templates/Layout.js
+++ src/templates/Layout.js
@@ -1,13 +1,13 @@
 import { Link, Outlet } from "react-router-dom";
 const Layout = () => {
     return ( <div>
-        <header className="flex items-center">
+        <header className="flex items-center justify-around">
         <Link className="text-red hover:animate-pulse font-bold text-3xl" to="/">
             Valhøj Skole
         </Link>
 
         <Link className="mt-5 flex items-center" to="https://www.rk.dk/forside">
-            <h1 className="font-bold hover:tracking-wider text-3xl">Rødovre Kommune</h1>
+            <h1 className="font-bold tracking-wider text-3xl">Rødovre Kommune</h1>
             <img src="" alt="RK logo" />
         </Link>
         </header>
